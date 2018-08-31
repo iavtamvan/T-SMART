966b5b8eda06ee29e9bf5aa14f3e5faf207999a3
root <ade.fajr.ariav@gmail.com>
2018-09-01 00:49:42 +0700

    #5

---
+++ app/src/main/java/com/iav/id/ituteam/model/newsModel/NewsModel.java
@@ -0,0 +1,42 @@
+package com.iav.id.ituteam.model.newsModel;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsModel {
+
+    @SerializedName("totalResults")
+    private int totalResults;
+
+    @SerializedName("articles")
+    private ArrayList<ArticlesItem> articles;
+
+    @SerializedName("status")
+    private String status;
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setArticles(ArrayList<ArticlesItem> articles) {
+        this.articles = articles;
+    }
+
+    public ArrayList<ArticlesItem> getArticles() {
+        return articles;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
